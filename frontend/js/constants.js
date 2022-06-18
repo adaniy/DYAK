0da8966c4ebd0cbbd9340413a3d60badcef4fb73
Adani YAKOBOU <53301498+adaniy@users.noreply.github.com>
2022-06-18 17:49:33 +0000

    Update002

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,23 +1,23 @@
-const contractAddress = "0xc8FDbc7FfAf31F4B2B08D08b4a53608bE873E925"; //Replace with your own contract address
+const contractAddress = "0xbe8408009cbbcdda2d4cca27fd29d106e5dac600"; //Replace with your own contract address
 const chain = 'polygon'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to DYAK NFT Project!!!";
+const welcome_h1 = "Welcome to The Rich Boys Club!!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'DRBC is a collection of black avatars taking on the Metaverse in fresh ways. With each series comes a host of adventures in unique NFTs styles.';
+const welcome_p = 'TRBC is a collection of black avatars taking on the Metaverse in fresh ways. With each series comes a host of adventures in unique NFTs styles.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
 const h2_presale_coming_soon = "Pre-Sale Minting Countdown";
 const h2_presale_mint = "Public Minting Countdown";
-const p_presale_coming_soon = "We are working hard to launch the NFT Collection. Stay tuned for DRBC updates!";
+const p_presale_coming_soon = "We are working hard to launch the NFT Collection. Stay tuned for TRBC updates!";
 const p_presale_mint_not_whitelisted = "You are not whitelisted for the pre-sale.. 😢";
 const p_presale_mint_whitelisted = "You're on the whitelist for the pre-sale! 🎉";
 const p_presale_mint_already_minted = "You've already claimed your whitelist mint. Thank you! 🎉";
 const p_public_mint = "No whitelist needed. Public minting is now open! 🎉";
 const button_presale_coming_soon = "Get on the Whitelist";
 const button_presale_mint_whitelisted = "Mint Your Special DYAK NFT";
-const button_presale_mint_not_whitelisted = "Get on the DRBC Whitelist";
-const button_presale_already_minted = "Join The DYAK Community";
+const button_presale_mint_not_whitelisted = "Get on the TRBC Whitelist";
+const button_presale_already_minted = "Join The TRBC Community";
 const button_public_mint = "Mint Your NFT";
 const mint_failed = "Minting failed. 😢 Please try again.";
 
